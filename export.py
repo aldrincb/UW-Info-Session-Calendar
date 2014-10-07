ff08fae6616b238752e1541006c268072a859a76
aldrincb <aldrin.balisi@gmail.com>
2014-10-06 22:17:39 -0400

    comment

--- export.py
+++ export.py
@@ -1,6 +1,7 @@
 import urllib2
 import re
 
+# the year and month are hard coded for testing
 string_to_parse = urllib2.urlopen("http://www.ceca.uwaterloo.ca/students/sessions_details.php?id=2014Oct").read()
 
 regex = re.compile("\">(.*)</")

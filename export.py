e072d0ccd894f418b3a0f585af17b8bf77638fda
aldrincb <aldrin.balisi@gmail.com>
2014-10-06 22:12:36 -0400

    Python script

---
+++ export.py
@@ -0,0 +1,12 @@
+import urllib2
+import re
+
+string_to_parse = urllib2.urlopen("http://www.ceca.uwaterloo.ca/students/sessions_details.php?id=2014Oct").read()
+
+regex = re.compile("\">(.*)</")
+r = regex.search(string_to_parse)
+r
+regex.match(string_to_parse)
+r.groups()
+r.groupdict()
+print regex.findall(string_to_parse)
